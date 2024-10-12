4533eddefeecca3dcdde4d71c546b7bb91976734
florian <florian.mehnert@gmail.com>
2024-10-12 10:56:41 +0200

    add delete flower view

--- flower_app/views.py
+++ flower_app/views.py
@@ -1,3 +1,4 @@
+from django.http import JsonResponse
 from django.shortcuts import render, redirect
 from .models import Flower
 from .forms import FlowerForm
@@ -15,6 +16,17 @@ def add(request):
         flowers = Flower.objects.all()  # Get all flowers from the database
         return render(request, 'flower_app/add.html', {'form': form, 'flowers': flowers})
     return render(request, 'flower_app/add.html', {'form': form})
+
+def delete_flower(request, flower_id):
+    if request.method == 'POST':
+        try:
+            flower = Flower.objects.get(id=flower_id)
+            flower.delete()
+            return JsonResponse({'status': 'success'})
+        except Flower.DoesNotExist:
+            return JsonResponse({'status': 'error', 'message': 'Flower not found'}, status=404)
+    return JsonResponse({'status': 'error', 'message': 'Invalid request method'}, status=400)
+
 
 
 def show(request):
--- flowerbed/urls.py
+++ flowerbed/urls.py
@@ -2,12 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from flower_app.views import add, show
+from flower_app.views import add, show, delete_flower
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', add, name='index'),
     path('add/', add, name='add'),
+    path('delete-flower/<int:flower_id>/', delete_flower, name='delete_flower'),
     path('show/', show, name='show'),
     path("__reload__/", include("django_browser_reload.urls")),
 ]
